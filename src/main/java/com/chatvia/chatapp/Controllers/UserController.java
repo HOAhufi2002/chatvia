dc50aca1ef9e3d1499dd53960263ae35d87bca94
minhhoa112222 <90520969+minhhoa112222@users.noreply.github.com>
2023-04-07 21:06:04 +0700

    Add files via upload

--- src/main/java/com/chatvia/chatapp/Controllers/UserController.java
+++ src/main/java/com/chatvia/chatapp/Controllers/UserController.java
@@ -9,15 +9,18 @@ package com.chatvia.chatapp.Controllers;
  * @author LeLan
  */
 
-import com.chatvia.chatapp.Models.Register;
 import com.chatvia.chatapp.Models.User;
-import com.chatvia.chatapp.Models.ViewedMessage;
 import com.chatvia.chatapp.Repo.UserRepository;
 import com.chatvia.chatapp.Repo.ViewedMessageRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+
+
 
 @Controller
 public class UserController {
@@ -35,38 +38,75 @@ public class UserController {
     @PostMapping("/register")
     public String registerHandle(@RequestParam String email, @RequestParam String fullname, @RequestParam String password, @RequestParam String repassword ) {
 
-//        session.setAttribute("email", user.username);
-//        session.setAttribute("fullname", user.fullname);
-        if (password.compareTo(repassword) != 0) {
-//            session.setAttribute("passwordError", "Mật khẩu không khớp!!!");
-            return "redirect:/register";
-        } else {
-            User user = userRepository.findByEmail(email);
+        if (password.length() < 6) {
+            return "redirect:/register?error=Password must be at least 6 characters long";
+        }
 
-            if (user != null) {
-//                session.setAttribute("emailError", "Email đã tồn tại!!!");
-                return "redirect:/register";
-            } else {
-                try {
-                    User add = new User();
-                    add.setEmail(email);
-                    add.setFullname(fullname);
-                    add.setPassword(password);
-                    userRepository.save(add);
-//                    session.setAttribute("msg_reg_sucesss", "Đăng ký tài khoản thành công!!!");
-                    return "redirect:/login";
-                } catch (Exception ex) {
-//                    session.setAttribute("error_reg", "Đăng ký tài khoản không thành công!!!");
-                    return "redirect:/register";
-                }
+        if (!email.matches("^\\S+@\\S+\\.\\S+$")) {
+            return "redirect:/register?error=Invalid email address";
+        }
 
-            }
+        if (!password.equals(repassword)) {
+            return "redirect:/register?error=Passwords do not match";
+        }
+
+        User user = userRepository.findByEmail(email);
+
+        if (user != null) {
+            return "redirect:/register?error=Email address already registered";
+        }
+
+        try {
+            User add = new User();
+            add.setEmail(email);
+            add.setFullname(fullname);
+            add.setPassword(password);
+            userRepository.save(add);
+            return "redirect:/login";
+        } catch (Exception ex) {
+            return "redirect:/register?error=Failed to register";
         }
     }
 
+    @GetMapping("/fail")
+    public String loginPageF(@RequestParam(required = false) String error) {
+        if (error != null) {
+            return "fail?error=" + error;
+        }
+        return "fail";
+    }
+
+    @PostMapping("/fail")
+    public String loginHandleF(@RequestParam String email, @RequestParam String password, RedirectAttributes redirectAttributes) {
+        User user = userRepository.findByEmail(email);
+        if (user != null && user.getPassword().equals(password)) {
+            return "redirect:/"; // redirect to home page or any other page you want
+        } else {
+            // Authentication failed, show error message
+            redirectAttributes.addFlashAttribute("error", "Invalid email or password");
+            return "redirect:/fail"; // redirect back to login page
+        }
+    }
     @GetMapping("/login")
-    public String loginpage() {
+    public String loginPage(@RequestParam(required = false) String error) {
+        if (error != null) {
+            return "login?error=" + error;
+        }
         return "login";
     }
+
+    @PostMapping("/login")
+    public String loginHandle(@RequestParam String email, @RequestParam String password, RedirectAttributes redirectAttributes) {
+        User user = userRepository.findByEmail(email);
+        if (user != null && user.getPassword().equals(password)) {
+            return "redirect:/"; // redirect to home page or any other page you want
+        } else {
+            // Authentication failed, show error message
+            redirectAttributes.addFlashAttribute("error", "Invalid email or password");
+            return "redirect:/fail"; // redirect back to login page
+        }
+    }
+
+
 
 }
